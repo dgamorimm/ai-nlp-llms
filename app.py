c1b3955d1d85bdcbfacb1032f96287e32b95cbea
Douglas Amorim <dpinheiro@multiplan.com.br>
2024-06-14 22:19:57 -0300

    vf

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import langchain
 import chromadb
 from flask import Flask, request, render_template
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader, AthenaLoader
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.llms.openai import OpenAI
@@ -15,6 +15,19 @@ warnings.filterwarnings('ignore')
 # Inicializa o Flask
 app = Flask(__name__)
 
+def athena_read(query):
+    database = ""
+    s3_output_uri = ''
+    profile_name = ''
+    athena_loader = AthenaLoader(
+        database=database,
+        profile_name=profile_name,
+        query=query,
+        s3_output_uri=s3_output_uri
+    )
+    documents = athena_loader.load()
+    return documents
+
 # Função para ler os arquivos em PDF
 def read_pdf(directory):
     file_loader = PyPDFDirectoryLoader(directory)
@@ -22,7 +35,9 @@ def read_pdf(directory):
     return documents
 
 # Executa a função para ler os arquivos em pdf
-doc = read_pdf('documents/')
+# doc = read_pdf('documents/')
+query = "SELECT t.* FROM brasil_io.dataset_covid19_casos_full t LIMIT 10000;"
+doc = athena_read(query)
 
 # Cria o gerador de embeddings (Coneverter o texto em representações numéricas = Tokenizador)
 embeddings = OpenAIEmbeddings(api_key = config('OPENAI_API_KEY'))
@@ -30,7 +45,9 @@ embeddings = OpenAIEmbeddings(api_key = config('OPENAI_API_KEY'))
 # Aplica o gerador de embeddings aos documentos e armazena no banco de dados vetorial ( quando inicializa o banco ele vai ficar em memória(demanda muito recurso computacional))
 # Esse banco vai armazenar os vetores em memória, mas da para ser utilizado persistindo em disco
 index_name = 'dgamorimm-index'
-index = Chroma.from_documents(documents = doc, embedding = embeddings, collection_name= index_name)
+index = Chroma.from_documents(documents = doc, embedding = embeddings, collection_name= index_name , persist_directory='data/')
+# index = Chroma(embedding_function = embeddings, collection_name= index_name , persist_directory='data/')
+# index.add_documents(documents = doc)
 
 # Define a função de busca por similaridade
 # k = 2 é para procurar os 2 vizinhos mais próximos aplicando o algoritmo de similaridade
